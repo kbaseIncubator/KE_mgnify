d864ab5c57d41f8c9828a16c998227bc7b490e5b
Jay R Bolton <jayrbolton@gmail.com>
2021-03-03 20:29:57 -0800

    Get things initially working

---
+++ src/mgnify_orm/main.py
@@ -0,0 +1,22 @@
+
+
+class Studies:
+
+    def __init__(self, url):
+        self.url = url
+        self.fetch()
+
+    def fetch(self):
+        pass
+
+
+class Downloads:
+
+    def __init__(self, url):
+        self.url = url
+
+
+class Analyses:
+
+    def __init__(self, url):
+        self.url = url
