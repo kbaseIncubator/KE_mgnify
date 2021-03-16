0a87256541444f735001a6ea8f8f4f1aaa12f474
Jay R Bolton <jayrbolton@gmail.com>
2021-03-16 15:32:14 -0700

    Download by sample, going through runs and analyses, rather than starting with studies

--- src/download_abundances/main.py
+++ src/download_abundances/main.py
@@ -11,7 +11,65 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=os.environ.get('LO
 
 def main(config: dict):
     _create_dirs(config)
-    _download_studies(config)
+    # _download_studies(config)
+    _download_samples(config)
+
+
+def _download_samples(config: dict) -> None:
+    # Iterate over every sample and jump to its latest analysis
+    samples_url = config['samples_url']
+    while True:
+        resp = requests.get(samples_url).json()
+        data = resp['data']
+        logging.info(f"Page {resp['meta']['pagination']['page']}:")
+        for sample_json in data:
+            _init_sample(sample_json, config)
+        break
+
+
+def _init_sample(sample_json: dict, config: dict) -> None:
+    sample_path = os.path.join(config['samples_dir'], sample_json['id'])
+    pathlib.Path(sample_path).mkdir(parents=True, exist_ok=True)
+    sample_json_path = os.path.join(sample_path, 'sample.json')
+    with open(sample_json_path, 'w') as fd:
+        json.dump(sample_json, fd)
+        logging.info(f"Wrote to {sample_json_path}")
+    # Fetch the runs in order to find the latest analysis
+    runs_url = sample_json['relationships']['runs']['links']['related']
+    runs = requests.get(runs_url).json()['data']
+    if len(runs) == 0:
+        logging.info("Sample has no runs, skipping")
+        return
+    run = runs[0]
+    # Fetch the analysis for the latest run
+    analysis_url = run['relationships']['analyses']['links']['related']
+    analyses = requests.get(analysis_url).json()['data']
+    if len(analyses) == 0:
+        logging.info(f"Sample {sample_json['id']} has no analyses, continuing")
+        return
+    analysis = analyses[0]
+    analysis_path = os.path.join(sample_path, 'analysis.json')
+    with open(analysis_path, 'w') as fd:
+        json.dump(analysis, fd)
+        logging.info(f"Wrote to {analysis_path}")
+    analysis_status = analysis['attributes']['analysis-status']
+    if analysis_status == 'failed':
+        logging.info("Analysis status is 'failed', continuing")
+        return
+    # Download the GO abundance table
+    go_url = analysis['relationships']['go-terms']['links']['related']
+    go_terms = requests.get(go_url).json()
+    go_path = os.path.join(sample_path, 'go-terms.json')
+    with open(go_path, 'w') as fd:
+        json.dump(go_terms, fd)
+        logging.info(f"Wrote GO term abundance to {go_path}")
+    # Download the taxonomy-SSU data
+    tax_ssu_url = analysis['relationships']['taxonomy-ssu']['links']['related']
+    tax_ssu = requests.get(tax_ssu_url).json()
+    tax_ssu_path = os.path.join(sample_path, 'taxonomy-ssu.json')
+    with open(tax_ssu_path, 'w') as fd:
+        json.dump(tax_ssu, fd)
+        logging.info(f"Wrote taxonomy SSU data to {tax_ssu_path}")
 
 
 def _download_studies(config: dict) -> None:
@@ -111,7 +169,8 @@ def _init_study_files(study_json: dict, analyses_json, samples_json: dict, confi
 
 
 def _create_dirs(config):
-    dirs = ('base_dir', 'studies_dir')
+    # dirs = ('base_dir', 'studies_dir')
+    dirs = ('base_dir', 'samples_dir')
     for name in dirs:
         path = config[name]
         pathlib.Path(path).mkdir(parents=True, exist_ok=True)
@@ -119,11 +178,15 @@ def _create_dirs(config):
 
 def _init_config(args):
     api_url = os.environ.get('API_URL', "https://www.ebi.ac.uk/metagenomics/api/v1").rstrip('/')
+    base_urls = requests.get(api_url).json()['data']
+    print(base_urls)
     conf = {
             'base_dir': args.dest,
             'studies_dir': os.path.join(args.dest, 'studies'),
+            'samples_dir': os.path.join(args.dest, 'samples'),
             'api_url': api_url,
-            'studies_url': api_url + '/studies',
+            'studies_url': base_urls['studies'],
+            'samples_url': base_urls['samples'],
             'experiment_type': os.environ.get('EXPERIMENT_TYPE', 'amplicon'),
             'file_substring': os.environ.get('FILE_SUBSTRING', 'GO_abundances'),
             'any_experiment_type': os.environ.get('ANY_EXPERIMENT') is not None,
